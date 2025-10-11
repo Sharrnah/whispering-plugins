b621f95df8fac7111ace7c7eec28177669b0941a
Sharrnah <55756126+Sharrnah@users.noreply.github.com>
2025-10-11 20:51:42 +0200

    [TASK][subtitle export] Add nemo canary model support to

--- Plugins/Export-Plugins/Subtitles-Export/subtitles_export_plugin.py
+++ Plugins/Export-Plugins/Subtitles-Export/subtitles_export_plugin.py
@@ -1,6 +1,6 @@
 # ============================================================
 # Subtitles Export Plugin for Whispering Tiger
-# V0.0.5
+# V0.0.6
 # See https://github.com/Sharrnah/whispering-ui
 # ============================================================
 #
@@ -9,6 +9,7 @@ import csv
 import os
 
 from faster_whisper.transcribe import Segment, TranscriptionInfo
+from Models.STT.nemo_canary import NemoCanary
 
 import Plugins
 import settings
@@ -41,6 +42,7 @@ class SubtitleExportPlugin(Plugins.Base):
         self.init_plugin_settings(
             {
                 # General
+                "model_type": {"type": "select", "value": "faster_whisper", "values": ["faster_whisper", "nemo_canary"]},
                 "language_spoken": {"type": "select", "value": "auto", "values": whisper_languages},
                 "language_txt_transcript": {"type": "select", "value": "", "values": source_text_translation_languages},
                 "language_target": {"type": "select", "value": "", "values": text_translation_languages},
@@ -51,7 +53,7 @@ class SubtitleExportPlugin(Plugins.Base):
                 "z_transcribe_button": {"label": "Start Transcription", "type": "button", "style": "primary"},
             },
             settings_groups={
-                "General": ["audio_filepath", "z_transcribe_button", "language_spoken", "language_txt_transcript", "language_target", "language_z_info_label", "subtitle_file", "subtitle_file_label"],
+                "General": ["model_type", "audio_filepath", "z_transcribe_button", "language_spoken", "language_txt_transcript", "language_target", "language_z_info_label", "subtitle_file", "subtitle_file_label"],
             }
         )
 
@@ -140,10 +142,76 @@ class SubtitleExportPlugin(Plugins.Base):
         with open(write_file, "w", encoding="utf-8") as f:
             _write_result(result, f)
 
-    def transcribe(self):
+    def use_model(self, audio_filepath, language) -> Tuple[Iterable[Segment]|dict, str]:
+        """
+        Use
+        Args:
+            audio_filepath: str, the path to the audio file to transcribe.
+            language: string or None, if None, the language will be detected automatically.
+
+        Returns:
+            transcribe_result: The transcription result, which is a tuple of segments and transcription info or dictionary. Must include start, end, text
+            detected_speaker_language: The language detected from the audio file or None if not detected.
+        """
+        model_type = self.get_plugin_setting("model_type")
         compute_dtype = settings.GetOption("whisper_precision")
         model = settings.GetOption("model")
         ai_device = settings.GetOption("ai_device")
+
+        transcribe_result_segments = None
+        detected_speaker_language = None
+
+        if self.audio_model is None:
+            match model_type:
+                case "faster_whisper":
+                    self.audio_model = faster_whisper.FasterWhisper(model, device=ai_device, compute_type=compute_dtype,
+                                                            cpu_threads=2, num_workers=2)
+                case "nemo_canary":
+                    self.audio_model = NemoCanary(device=ai_device, compute_type=compute_dtype)
+                    self.audio_model.load_model(model, device=ai_device, compute_type=compute_dtype)
+
+
+        if self.audio_model is not None:
+            try:
+                match model_type:
+                    case "faster_whisper":
+                        transcribe_result_segments, audio_info = self.audio_model.model.transcribe(audio_filepath, task="transcribe",
+                                                                            language=language,
+                                                                            condition_on_previous_text=True,
+                                                                            initial_prompt=None,
+                                                                            log_prob_threshold=-1.0,
+                                                                            no_speech_threshold=0.6,
+                                                                            temperature=[
+                                                                                0.0,
+                                                                                0.2,
+                                                                                0.4,
+                                                                                0.6,
+                                                                                0.8,
+                                                                                1.0,
+                                                                            ],
+                                                                            beam_size=5,
+                                                                            #word_timestamps=True,
+                                                                            word_timestamps=False,
+                                                                            without_timestamps=False,
+                                                                            patience=1.0
+                                                                            )
+                        detected_speaker_language = audio_info.language
+                    case "nemo_canary":
+                        transcribe_result = self.audio_model.long_form_transcribe(audio_filepath,
+                                                                                      task="transcribe",
+                                                                                      source_lang=language,
+                                                                                      target_lang=language,
+                                                                                      without_timestamps=False
+                                                                                      )
+                        transcribe_result_segments = transcribe_result["segments"]
+                        detected_speaker_language = language
+            except Exception as e:
+                print(e)
+                websocket.BroadcastMessage(json.dumps({"type": "error", "data": "Generating subtitle file failed:\n" + str(e)}))
+
+        return transcribe_result_segments, detected_speaker_language
+
+    def transcribe(self):
         audio_filepath = self.get_plugin_setting("audio_filepath")
         write_file = self.get_plugin_setting("subtitle_file")
         whisper_language = self.get_plugin_setting("language_spoken")
@@ -171,81 +239,54 @@ class SubtitleExportPlugin(Plugins.Base):
             websocket.BroadcastMessage(json.dumps({"type": "error", "data": f"Unsupported file extension: {file_extension}"}))
             return  # Exit the function
 
-        if self.audio_model is None:
-            self.audio_model = faster_whisper.FasterWhisper(model, device=ai_device, compute_type=compute_dtype,
-                                                            cpu_threads=2, num_workers=2)
+        websocket.set_loading_state("subtitle_export_plugin_working", True)
+        result_segments, language = self.use_model(audio_filepath, whisper_language)
+        if result_segments is not None:
+            if target_language != "":
+                translated_segments = []
 
-        if self.audio_model is not None:
-            websocket.set_loading_state("subtitle_export_plugin_working", True)
+                if text_language == "" or text_language == "auto-by-speech" or text_language == "auto-by-text":
+                    if text_language == "auto-by-speech" and whisper_language is None:
+                        text_language = language
+                    elif text_language == "auto-by-text" and whisper_language is None:
+                        text_language = "auto"
+                    else:
+                        text_language = whisper_language
 
-            try:
-                result_segments, audio_info = self.audio_model.model.transcribe(audio_filepath, task="transcribe",
-                                                                                language=whisper_language,
-                                                                                condition_on_previous_text=True,
-                                                                                initial_prompt=None,
-                                                                                log_prob_threshold=-1.0,
-                                                                                no_speech_threshold=0.6,
-                                                                                temperature=[
-                                                                                    0.0,
-                                                                                    0.2,
-                                                                                    0.4,
-                                                                                    0.6,
-                                                                                    0.8,
-                                                                                    1.0,
-                                                                                ],
-                                                                                beam_size=5,
-                                                                                #word_timestamps=True,
-                                                                                word_timestamps=False,
-                                                                                without_timestamps=False,
-                                                                                patience=1.0
-                                                                                )
-                if target_language != "":
-                    translated_segments = []
+                    # get ISO3 language code
+                    if whisper_language in texttranslate.texttranslateNLLB200_CTranslate2.LANGUAGES_ISO1_TO_ISO3:
+                        text_language = texttranslate.texttranslateNLLB200_CTranslate2.LANGUAGES_ISO1_TO_ISO3[whisper_language][0]
 
-                    if text_language == "" or text_language == "auto-by-speech" or text_language == "auto-by-text":
-                        if text_language == "auto-by-speech" and whisper_language is None:
-                            text_language = audio_info.language
-                        elif text_language == "auto-by-text" and whisper_language is None:
-                            text_language = "auto"
-                        else:
-                            text_language = whisper_language
+                id = 0
+                for segment in result_segments:
+                    translation, _, _ = texttranslate.TranslateLanguage(
+                        segment.text,
+                        text_language,
+                        target_language,
+                        as_iso1=False
+                    )
+                    translated_segments.append(Segment(
+                        #id=segment.id,
+                        id=id,
+                        #seek=segment.seek,
+                        start=segment.start,
+                        end=segment.end,
+                        text=translation,
+                        #temperature=segment.temperature,
+                        #avg_logprob=segment.avg_logprob,
+                        #no_speech_prob=segment.no_speech_prob,
+                        #compression_ratio=segment.compression_ratio,
+                        #tokens=segment.tokens,
+                        #words=segment.words
+                    ))
+                    id += 1
+                result_segments = translated_segments
 
-                        # get ISO3 language code
-                        if whisper_language in texttranslate.texttranslateNLLB200_CTranslate2.LANGUAGES_ISO1_TO_ISO3:
-                            text_language = texttranslate.texttranslateNLLB200_CTranslate2.LANGUAGES_ISO1_TO_ISO3[whisper_language][0]
-
-                    for segment in result_segments:
-                        translation, _, _ = texttranslate.TranslateLanguage(
-                            segment.text,
-                            text_language,
-                            target_language,
-                            as_iso1=False
-                        )
-                        translated_segments.append(Segment(
-                            id=segment.id,
-                            seek=segment.seek,
-                            start=segment.start,
-                            end=segment.end,
-                            text=translation,
-                            temperature=segment.temperature,
-                            avg_logprob=segment.avg_logprob,
-                            no_speech_prob=segment.no_speech_prob,
-                            compression_ratio=segment.compression_ratio,
-                            tokens=segment.tokens,
-                            words=segment.words
-                        ))
-                    result_segments = translated_segments
-
-                # Call the method associated with the file extension
-                write_method(result_segments)
-
-            except Exception as e:
-                print(e)
-                websocket.BroadcastMessage(json.dumps({"type": "error", "data": "Generating subtitle file failed:\n" + str(e)}))
-
-            websocket.set_loading_state("subtitle_export_plugin_working", False)
-
+            # Call the method associated with the file extension
+            write_method(result_segments)
             websocket.BroadcastMessage(json.dumps({"type": "info", "data": "Generating subtitle file finished."}))
+
+        websocket.set_loading_state("subtitle_export_plugin_working", False)
 
     def on_event_received(self, message, websocket_connection=None):
         if self.is_enabled(False):
